2ae01c472bf8113a750c3e06843b405ffbf84061
ewsmith1 <63798599+ewsmith1@users.noreply.github.com>
2021-10-06 17:02:55 +0100

    add shapefiles for all UK postcodes

--- honoursproject/src/3_seroprevalence_postcode.R
+++ honoursproject/src/3_seroprevalence_postcode.R
@@ -1,2 +1,11 @@
-library(geojsonio)
-spdf <- geojson_read("./data/G_postcodes.geojson", what = "sp")
+# Read this shape file with the rgdal library.
+library(rgdal)
+spdf <- readOGR(
+    dsn = paste0(getwd(), "/honoursproject/data/Shapefiles"),
+    layer = "Districts",
+    verbose = FALSE
+)
+
+# Basic plot of shape file
+par(mar = c(0, 0, 0, 0))
+plot(spdf, col = "#f2f2f2", bg = "skyblue", lwd = 0.25, border = 0)

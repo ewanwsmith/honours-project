94d3191e252ec4c9a8c6a5f265384f435a9a1e2b
ewsmith1 <63798599+ewsmith1@users.noreply.github.com>
2021-10-06 18:40:48 +0100

    empty map of G postcodes present in the dataset

--- honoursproject/src/3_seroprevalence_postcode.R
+++ honoursproject/src/3_seroprevalence_postcode.R
@@ -1,7 +1,7 @@
 # read in study dataset
 study_dat <- read.csv(file = "./honoursproject/data/joined_data.csv", header = TRUE)
 
-# Read UK postcode shape file with the rgdal library.
+# Read UK postcode shape file
 library(rgdal)
 spdf <- readOGR(
     dsn = paste0(getwd(), "/honoursproject/data/Shapefiles"),
@@ -9,4 +9,11 @@ spdf <- readOGR(
     verbose = FALSE
 )
 
-spdf <- spdf[spdf@data %in% study_dat$PostcodePart, ]
+# filter for just postcode areas within the study dataset
+spdf <- subset(spdf, spdf@data$name %in% study_dat$PostcodePart)
+plot(spdf)
+
+library(stringr)
+# filter for just Glasgow postcodes
+spdf <- subset(spdf, startsWith(spdf@data$name, "G"))
+plot(spdf)

1e6648b37c4ffa1fc4788684deea85e0c4c3624a
ewsmith1 <63798599+ewsmith1@users.noreply.github.com>
2022-01-21 13:44:56 +0000

    age plot

--- honoursproject/src/8_neut_age.R
+++ honoursproject/src/8_neut_age.R
@@ -15,10 +15,11 @@ age_jitter_boxplot = ggplot(data = S1train, aes(x=as.factor(RBD_seropositive), y
     geom_jitter(color="black", size=0.4, alpha=0.9)
 
 # too many datapoints, so density plot
+
 library(ggpubr)
 age_density_plot = ggplot(data=S1train, aes(x=Age, group=as.factor(RBD_seropositive), fill=as.factor(RBD_seropositive))) +
     geom_density(adjust=1.5, alpha=.4) +
-    theme_minimal() +
+  theme_minimal(base_size = 30) + 
     theme(legend.position="bottom", legend.box = "vertical") +
     guides(fill=guide_legend(title="RBD Seropositivity"))
 
@@ -30,6 +31,14 @@ sink()
 age_probit_plot = ggplot(S1train, aes(x=Age, y=RBD_seropositive)) + 
   geom_point() + 
   stat_smooth(method="glm", method.args=list(family=binomial(link="probit"))) +
-  theme_minimal() +
+  theme_minimal(base_size = 30) + 
   ylab("RBD Seropositivity")
 
+png(
+    file = "./honoursproject/src/plots/age_plot.png",
+    width = 2400, height = 1200
+)
+library(ggpubr)
+age_plot = ggarrange(age_density_plot, age_probit_plot, ncol = 2, labels = c("A", "B"))
+  plot(age_plot)
+dev.off()
